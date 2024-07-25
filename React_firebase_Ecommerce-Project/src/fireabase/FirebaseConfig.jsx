31f0a5fa4052d90ac8967f25cc011c9b9ec324f7
rdsandrds <rohitsolunke1995@gmail.com>
2024-07-25 23:47:30 +0530

    commit_25072024

---
+++ React_firebase_Ecommerce-Project/src/fireabase/FirebaseConfig.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+import {getAuth} from 'firebase/auth';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+ 
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const fireDB = getFirestore(app);
+const auth = getAuth(app);
+
+export {fireDB, auth}
